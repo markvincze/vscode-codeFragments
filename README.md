aa89279dbe66953ea08f0e8c8821c5b225b59101
markvincze <mrk.vincze@gmail.com>
2017-11-25 18:39:24 +0100

    Edit README, remove unnecessary files

--- README.md
+++ README.md
@@ -1,65 +1,19 @@
-# hello README
+# Code Fragments extension for Visual Studio Code
 
-This is the README for your extension "hello". After writing up a brief description, we recommend including the following sections.
+With this extension you can save static code fragments for later use, and easily insert them into source files whenever needed.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+After installing the extension, a new section called "CODE FRAGMENTS" is added to the Explorer, which shows a list of the previously saved fragments. Clicking on one inserts its content into the current editor at the cursor position.
 
-For example if there is an image subfolder under your extension project workspace:
+You can save a piece of code by selecting it in the editor, and then executing the "Save selection as Code Fragment" from the Command Palette (brought up with `Ctrl+Shift+P`) or in the context menu.
 
-\!\[feature X\]\(images/feature-x.png\)
+Saving a selection as a Code Fragment:
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
-
-## Requirements
-
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
-
-## Extension Settings
-
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
-
-## Known Issues
-
-Calling out known issues can help limit users opening duplicate issues against your extension.
+Inserting a Code Fragment to the current cursor position:
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
 ### 1.0.0
 
-Initial release of ...
-
-### 1.0.1
-
-Fixed issue #.
-
-### 1.1.0
-
-Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on OSX or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on OSX or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (OSX) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+Initial release of Code Fragments.
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,4 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { CodeFragmentProvider } from './codeFragments';
 
@@ -9,57 +7,55 @@ import { CodeFragmentProvider } from './codeFragments';
 export function activate(context: vscode.ExtensionContext) {
     const codeFragmentProvider = new CodeFragmentProvider(context);
 
+    const saveSelectedCodeFragment = () => {
+        const showNoTextMsg = () => vscode.window.showInformationMessage(
+            'Select a piece of code in the editor to save it as a fragment.');
+
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            showNoTextMsg();
+            return;
+        }
+
+        editor.edit(builder => {
+            const content = editor.document.getText(editor.selection);
+
+            if (content.length < 1) {
+                showNoTextMsg();
+                return;
+            }
+
+            codeFragmentProvider.saveNewCodeFragment(content);
+        });
+    };
+
+    const insertCodeFragment = fragmentId => {
+        if (!fragmentId) {
+            vscode.window.showInformationMessage(
+                'Insert a code fragment into the editor by clicking on it in the Code Fragments view.');
+        }
+
+        const editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            vscode.window.showInformationMessage('Open a file in the editor to insert a fragment.');
+            return;
+        }
+
+        const content = codeFragmentProvider.getFragmentContent(fragmentId);
+
+        editor.edit(builder => {
+            builder.insert(editor.selection.start, content);
+        });
+    };
+
     codeFragmentProvider
         .initialize()
         .then(() => {
             vscode.window.registerTreeDataProvider('codeFragments', codeFragmentProvider);
 
-            let disposable = vscode.commands.registerCommand('codeFragments.saveSelectedCodeFragment', () => {
-                const showNoTextMsg = () => vscode.window.showInformationMessage(
-                    'Select a piece of code in the editor to save it as a fragment.');
-
-                const editor = vscode.window.activeTextEditor;
-                if (!editor) {
-                    showNoTextMsg();
-                    return;
-                }
-
-                editor.edit(builder => {
-                    const content = editor.document.getText(editor.selection);
-
-                    if (content.length < 1) {
-                        showNoTextMsg();
-                        return;
-                    }
-
-                    codeFragmentProvider.saveNewCodeFragment(content);
-                });
-            });
-
-            context.subscriptions.push(disposable);
-
-            disposable = vscode.commands.registerCommand('codeFragments.insertCodeFragment', fragmentId => {
-                if (!fragmentId) {
-                    vscode.window.showInformationMessage(
-                        'Insert a code fragment into the editor by clicking on it in the Code Fragments view.');
-                }
-
-                const editor = vscode.window.activeTextEditor;
-                if (!editor) {
-                    vscode.window.showInformationMessage('Open a file in the editor to insert a fragment.');
-                    return;
-                }
-
-                const content = codeFragmentProvider.getFragmentContent(fragmentId);
-
-                editor.edit(builder => {
-                    builder.insert(editor.selection.start, content);
-                });
-            });
-
-            context.subscriptions.push(disposable);
-        }
-    );
+            context.subscriptions.push(vscode.commands.registerCommand('codeFragments.saveSelectedCodeFragment', saveSelectedCodeFragment));
+            context.subscriptions.push(vscode.commands.registerCommand('codeFragments.insertCodeFragment', insertCodeFragment));
+        });
 }
 
 export function deactivate() { }
